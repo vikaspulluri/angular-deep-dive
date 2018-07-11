f7a790122912d2abff93cce312d98d2b9f7ca0e3
vikaspulluri <vikasiiitn@gmail.com>
2018-07-12 00:20:05 +0530

    Routers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,37 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:'server',name:'test-server',content:'Just a test server'}];
-  data = "parent data";
-
-  oddNumber = false;
-  value = 5;
-  numbers = [1,2,3,4,5,6];
-  onServerAdded(serverData: {serverName: string, serverContent: string}){
-  	this.serverElements.push({
-  		type: 'server',
-  		name: serverData.serverName,
-  		content: serverData.serverContent
-  	});
-  }
-
-  onBluePrintAdded(blueprintData: {serverName: string, serverContent: string}){
-  	this.serverElements.push({
-  		type: 'blueprint',
-  		name: blueprintData.serverName,
-  		content: blueprintData.serverContent
-  	})
-  }
-
-  changeData(){
-    this.data = "parent data changed!!!";
-  }
-
-  destroyComponent(){
-    //Won't applicable to primitive data types.
-    //applicable in case of array and emptying the array
-    //when *ngFor try to repeat the component template, since arr.length=0, component won't render
-    //then ngOnDestroy will triggered
-    this.data = null;
-  }
 }
